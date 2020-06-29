b9ad8b44bcc96f32a5eb8369a5c5bfbd9b00e642
hasyrails <runteq.learning.hasy@gmail.com>
2020-06-29 20:27:23 +0900

    [feat] storeを作成
    createStore(rootReducer) :storeを作成
    <Provider>コンポーネント: storeの情報を全てのコンポーネントから参照可能に

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import CalenderBoard from "./components/CalenderBoard";
 
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
-
 dayjs.locale("ja");
 
+import rootReducer from "./redux/rootReducer";
+
+const store = createStore(rootReducer);
+
 const App = () => (
-  <div>
+  <Provider store={store}>
     <CalenderBoard/>
-  </div>
+  </Provider>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
