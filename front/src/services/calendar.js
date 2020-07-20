1589f0134b7ac869ab89843e396fc180fc410118
hasyrails <runteq.learning.hasy@gmail.com>
2020-07-20 21:30:36 +0900

    [feat] issue#5/ 前月/次月を取得する関数実装
    getNextMonth
    getPreviousMonth
    formatMonth

--- front/src/services/calendar.js
+++ front/src/services/calendar.js
@@ -18,6 +18,21 @@ export const getMonth = ({ year, month }) => {
   return dayjs(`${year}-${month}`);
 }
 
+export const getNextMonth = month => {
+  const day = getMonth(month).add(1, "month");
+  return formatMonth(day);
+}
+
+export const getPreviousMonth = month => {
+  const day = getMonth(month).add(-1, "month");
+  return formatMonth(day);
+}
+
+export const formatMonth = day => ({
+  month: day.month() + 1,
+  year: day,year()
+});
+
 export const isSameDay = (d1, d2) => {
   const format = "YYYYMMDD";
   return d1.format(format) === d2.format(format);
