949b6e4a902dd82dba1d1a9acda1bddf0e3c17da
hasyrails <runteq.learning.hasy@gmail.com>
2020-07-29 07:06:46 +0900

    issue #23,24/ 予定追加ダイアログを開/閉機能を追加

--- front/src/components/AddScheduleDialog/container.jsx
+++ front/src/components/AddScheduleDialog/container.jsx
@@ -1,7 +1,19 @@
-import AddScheduleDialog from "./presentation";
-
 import { connect } from "react-redux";
+import AddScheduleDialog from "./presentation";
+import { addScheduleCloseDialog } from "../../redux/addSchedule/actions";
 
-const mapStateToProps = state => ({ schedule: state.addSchedule });
 
-export default connect(mapStateToProps)(AddScheduleDialog);
+const mapStateToProps = state => ({ 
+  schedule: state.addSchedule
+ });
+
+const mapDispatchToProps = dispatch => ({
+  closeDialog: () => {
+    dispatch(addScheduleCloseDialog());
+  }
+ });
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddScheduleDialog);
