37168f6a386cf07da9c0532c6d023ba6db99c42c
hasyrails <runteq.learning.hasy@gmail.com>
2020-07-29 22:28:55 +0900

    issue #30,#31 / redux管理・DatePicker

--- front/src/components/AddScheduleDialog/presentation.jsx
+++ front/src/components/AddScheduleDialog/presentation.jsx
@@ -9,7 +9,13 @@ import {
   Grid
 } from "@material-ui/core";
 
-import { LocationOnOutlined, NotesOutlined, InputOutlined } from "@material-ui/icons";
+import { 
+  LocationOnOutlined,
+  NotesOutlined,
+  AccessTime
+} from "@material-ui/icons";
+
+import { DatePicker } from "@material-ui/pickers";
 import { withStyles } from "@material-ui/styles";
 
 const spacer = { margin: "4px 0" };
@@ -18,33 +24,56 @@ const Title = withStyles({
   root: { marginBottom: 32, fontSize: 22 }
 })(Input);
 
-const AddScheduleDialog = ({ schedule: { isDialogOpen }, closeDialog }) => {
+const AddScheduleDialog = ({ 
+  schedule: {
+    form: { title, location, description },
+    isDialogOpen
+  },
+  closeDialog,
+  setSchedule
+}) => {
   return (
     <Dialog open={isDialogOpen} onClose={closeDialog} maxWidth="xs" fullWidth>
       <DialogContent>
-        <Title autoFocus fullWidth placeholder="タイトルと日時を追加" />
+        <Title
+          autoFocus
+          fullWidth
+          value={title}
+          placeholder="タイトルと日時を追加"
+          onChange={e => setSchedule({ title: e.target.value })}
+        />
+        <Grid container spacing={1} alignItems="center" justify="space-between">
+          <Grid item>
+            <AccessTime />
+          </Grid>
+          <Grid item xs={10}>
+            <DatePicker
+              variant="inline"
+              format="YYYY年M月D日"
+              animateYearScrolling
+              disableToolbar
+              fullWidth
+              style={spacer}
+            />
+          </Grid>
+        </Grid>
         <Grid container spacing={1} alignItems="center" justify="space-between">
           <Grid item>
             <LocationOnOutlined />
           </Grid>
           <Grid item xs={10}>
-            <TextField style={spacer} fullWidth placeholder="説明を追加" />
+            <TextField
+              style={spacer}
+              fullWidth
+              placeholder="場所を追加"
+              value={location}
+              onChange={e => setSchedule({ location: e.target.value })}
+            />
           </Grid>
         </Grid>
-        <Grid container spacing={1} alignItems="center" justify="space-between">
-          <Grid item>
-            <NotesOutlined />
-          </Grid>
-          <Grid item xs={10}>
-            <TextField style={spacer} fullWidth placeholder="説明を追加" />
-          </Grid>
-        </Grid>
+        ...
       </DialogContent>
-      <DialogActions>
-        <Button color="primary" variant="outlined">
-          保存
-        </Button>
-      </DialogActions>
+      <DialogActions>...</DialogActions>
     </Dialog>
   );
 };
