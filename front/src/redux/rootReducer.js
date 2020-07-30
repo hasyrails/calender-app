fc38c877bfbee2921e7f2d7b1c4a12d882725449
hasyrails <runteq.learning.hasy@gmail.com>
2020-07-31 05:05:24 +0900

    issue #44 / currentScheduleコンポーネントのrootReducerを設定

--- front/src/redux/rootReducer.js
+++ front/src/redux/rootReducer.js
@@ -5,11 +5,13 @@ import calendarReducer from "./calendar/reducer";
 import addScheduleReducer from "./addSchedule/reducer";
 
 import schedulesReducer from "./schedules/reducer";
+import currentScheduleReducer from "./currentSchedule/reducer";
 
 const rootReducer = combineReducers({    
   calendar: calendarReducer,
   addSchedule: addScheduleReducer,
-  schedules: schedulesReducer
+  schedules: schedulesReducer,
+  currentSchedule: currentScheduleReducer
 });
 
 export default rootReducer;
